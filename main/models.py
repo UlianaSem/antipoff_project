20e7134023085f5e3256c69572b58f9acfccc6fc
Uliana Semyonova <i@ulianasemyonova.ru>
2024-02-03 15:45:45 +0500

    Fix

--- main/models.py
+++ main/models.py
@@ -1,12 +1,6 @@
 from django.db.models import Model, DecimalField, CharField, ForeignKey, BooleanField, CASCADE
 
 
-"""Написать сервис, который принимает запрос с указанием кадастрового номера, широты и долготы, эмулирует отправку 
-запроса на внешний сервер, который может обрабатывать запрос до 60 секунд. Затем должен отдавать результат запроса. 
-Считается, что внешний сервер может ответить true или false.
-Данные запроса на сервер и ответ с внешнего сервера должны быть сохранены в БД. Нужно написать АПИ для получения
- истории всех запросов/истории по кадастровому номеру."""
-
 class Request(Model):
     cadastral_number = CharField(max_length=15, verbose_name='кадастровый номер')
     latitude = DecimalField(max_digits=6, decimal_places=2, verbose_name='широта')

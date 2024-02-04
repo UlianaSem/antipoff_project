cc9c5feba668ca29210a992f16274bb75c39b727
Uliana Semyonova <i@ulianasemyonova.ru>
2024-02-04 18:38:25 +0500

    Create create_admin command

---
+++ main/management/commands/create_admin.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        User.objects.create_user(
+            username='admin',
+            password='admin',
+            is_staff=True,
+            is_superuser=True,
+        )

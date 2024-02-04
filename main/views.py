1000a93e36b4a7f699905bee06e761f7b78e5415
Uliana Semyonova <i@ulianasemyonova.ru>
2024-02-04 19:34:53 +0500

    Add docs

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 import threading
 
-from rest_framework import status
+from drf_spectacular.utils import extend_schema, OpenApiParameter, inline_serializer
+from rest_framework import status, serializers
 from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
@@ -11,6 +12,9 @@ from main.serializers import RequestCreateSerializer, RequestSerializer
 from main.services import emulate_sending
 
 
+@extend_schema(
+    summary="Создать запрос",
+)
 class RequestAPIView(CreateAPIView):
     queryset = Request.objects.all()
     serializer_class = RequestCreateSerializer
@@ -21,11 +25,33 @@ class RequestAPIView(CreateAPIView):
         thread.start()
 
 
+@extend_schema(
+    summary="Запросить результат",
+    parameters=[
+        OpenApiParameter(
+            name='id',
+            location=OpenApiParameter.PATH,
+            description='id запроса',
+            required=True,
+            type=int)
+        ],
+)
 class AnswerAPIView(RetrieveAPIView):
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
 
 
+@extend_schema(
+    summary="Посмотреть историю",
+    parameters=[
+        OpenApiParameter(
+            name='search',
+            location=OpenApiParameter.QUERY,
+            description='Фильтр по кадастровому номеру',
+            required=False,
+            type=str)
+    ],
+)
 class HistoryAPIView(ListAPIView):
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
@@ -33,6 +59,17 @@ class HistoryAPIView(ListAPIView):
     search_fields = ['cadastral_number']
 
 
+@extend_schema(
+        summary="Проверить доступность сервера",
+        responses={
+            status.HTTP_200_OK: inline_serializer(
+                name='PingResponse',
+                fields={
+                    'message': serializers.CharField(),
+                }
+            )
+        },
+)
 @api_view(http_method_names=['GET'])
 def get_ping(request):
     threads = [thread.name for thread in threading.enumerate()]
